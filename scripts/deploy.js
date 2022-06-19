64986b4e3729d329866efc856a7bc154b222794d
Taxmann Technologies <taxmanntechnologies@Taxmanns-MacBook-Pro.local>
2022-06-19 21:10:17 +0530

    created first nft

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+    //0x139582Fc1D44A58b1ed0Bc68Da315c0430f13136
---
+++ scripts/mint-nft.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const API_URL = process.env.API_URL;
+
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+console.log(JSON.stringify(contract.abi));
+
+const contractAddress = "0x139582Fc1D44A58b1ed0Bc68Da315c0430f13136";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+//create transaction
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+}
+mintNFT(
+  "https://gateway.pinata.cloud/ipfs/QmVBZ7D5JwA8zL8G8sJg1oQ6H91PUSwqoLF2T8YNUxjvBh"
+);
+
+//0x6320b100a8aefca4e81fe493068c7d9f43c397d25b275c2092995c536cd598bb
